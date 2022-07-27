e0c4394a945627be0b11479c5a95bce2cc0134d7
Travis Offtermatt <travisty2@gmail.com>
2022-07-27 00:04:40 -0600

    updating version

--- browser/update.go
+++ browser/update.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	Version = "0.1.1"
+	Version = "0.1.2"
 
 	tagUrl = `https://api.github.com/repos/KiameV/ffprModManager/tags`
 	relUrl = `https://github.com/KiameV/ffprModManager/releases/%s`
