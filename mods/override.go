2fefddb12ac8a73358bb81285c3e8986752d0490
Travis Offtermatt <travisty2@gmail.com>
2022-08-14 11:20:49 -0600

    adding Preview to override

--- mods/override.go
+++ mods/override.go
@@ -2,12 +2,16 @@ package mods
 
 type Override struct {
 	NexusModID     string            `json:"id" xml:"id"`
+	Preview        *Preview          `json:"preview" xml:"preview"`
 	Description    *string           `json:"description,omitempty" xml:"description,omitempty"`
 	ReleaseNotes   *string           `json:"releaseNotes,omitempty" xml:"releaseNotes,omitempty"`
 	Configurations *[]*Configuration `json:"configurations,omitempty" xml:"configurations,omitempty"`
 }
 
 func (o Override) Override(m *Mod) {
+	if o.Preview != nil {
+		m.Preview = o.Preview
+	}
 	if o.Description != nil {
 		m.Description = *o.Description
 	}
